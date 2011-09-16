4607510add5aaf6c68d9ae105b11e4f509a51409
prasanna.tuladhar <prasanna.tuladhar@WS0668.wirecard.lan>
2011-09-16 15:29:59 +0200

    initial

---
+++ ecom/src/main/java/com/ecom/web/main/EcomApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.web.main;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class EcomApplication extends WebApplication {
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ecom/src/main/java/com/ecom/web/main/HomePage.java
@@ -0,0 +1,15 @@
+package com.ecom.web.main;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 6571024929658974042L;
+
+	public HomePage() {
+		super();
+
+	}
+
+	
+}
