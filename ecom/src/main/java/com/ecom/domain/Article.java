169e7014dd81456a7cd4d69598f8a2781afc829b
Prasanna Tuladhar <prasanna.tuladhar@gmail.com>
2011-09-26 17:02:56 +0200

    #cahnged library and pom

--- ecom/src/main/java/com/ecom/domain/Article.java
+++ ecom/src/main/java/com/ecom/domain/Article.java
@@ -2,7 +2,7 @@ package com.ecom.domain;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.document.mongodb.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Article {
