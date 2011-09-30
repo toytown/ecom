fd7bf08098f01012d856f4afa2608a63b13c45a0
home <home@pt-laptop>
2011-09-30 06:03:01 +0200

    ADDED NEW DOMAIN OBJECTS

---
+++ ecom/src/main/java/com/ecom/domain/Category.java
@@ -0,0 +1,15 @@
+package com.ecom.domain;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 6330792452715523916L;
+
+	private Long id;
+	
+	private String description;
+
+//	private List<CategoryType> categoryTypes = new ArrayList<CategoryType>(0);;
+	
+}
--- ecom/src/main/java/com/ecom/domain/RealState.java
+++ ecom/src/main/java/com/ecom/domain/RealState.java
@@ -1,5 +1,7 @@
 package com.ecom.domain;
 
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,8 +12,92 @@ public class RealState {
 	@Id
 	private ObjectId _id;
 	private String title;
-	private Short status;
-	private String description;
+	private int status;
 	private int originalPrice;
+	private String type;
+
+	private String areaCode;
+
+	private String city;
+
+	private String street;
+
+	private String houseNo;
+
+	private Double size;
+
+	private Double cost;
+
+	private Double floor;
+
+	private Double totalFloors;
+
+	private Double totalRooms;
+
+	private Short bedRooms;
+
+	private Short bathRooms;
+
+	private Boolean toiletWithBathRoom;
+
+	private Boolean cellarAvailable;
+
+	private Boolean balconyAvailable;
+
+	private Boolean liftAvailable;
+
+	private Boolean gardenAvailable;
+
+	private Short condition;
+
+//	private CategoryType categoryType;
+
+//	private HeatingType heatingType;
+
+	private Double additionalCost;
+
+	private Boolean heatingCostIncluded;
+
+	private Double depositPeriod;
+
+	private Date availableFrom;
+
+	private Boolean garageAvailable;
+
+	private Double garageCost;
+
+	private String description;
+
+	private String areaDescription;
+
+	private String fittings;
+
+	private Boolean energyPassAvailable;
+
+	private Boolean kitchenAvailable;
+
+	private Boolean furnished;
+
+	private Boolean animalsAllowed;
+
+	private Integer builtYear;
+
+	private String lastRenovatedYear;
+
+	private String otherInformation;
+
+	private Boolean provisionFree;
+
+	private String provisionCondition;
+
+	private String imageDir;
+
+	private Boolean barrierFree;
+
+	private Boolean seniorAppartment;
+
+	private User user;
+
+//	private Category category;
 	
 }
