9749ff1c6f25d89f31f0432287adb402907c11ce
Prasanna Tuladhar <prasanna.tuladhar@gmail.com>
2011-10-06 13:31:48 +0200

    added catgeory

---
+++ ecom/src/main/java/com/ecom/domain/CategoryType.java
@@ -0,0 +1,34 @@
+package com.ecom.domain;
+
+import java.io.Serializable;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class CategoryType implements Serializable {
+
+    private static final long serialVersionUID = -3266024762221707686L;
+
+    @Id
+    private ObjectId id;
+
+    private String name;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
