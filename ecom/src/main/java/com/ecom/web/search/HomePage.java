852b2b899132d8d75e8e46f95300a4a21b966dd2
home <home@192.168.1.35>
2011-10-07 13:24:40 +0200

    #added submit button

--- ecom/src/main/java/com/ecom/web/search/HomePage.java
+++ ecom/src/main/java/com/ecom/web/search/HomePage.java
@@ -1,5 +1,8 @@
 package com.ecom.web.search;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 
@@ -16,19 +19,43 @@ public class HomePage extends GenericTemplatePage {
 
 		
 	    SearchRequest req = new SearchRequest();
-	    CompoundPropertyModel searchReqModel = new CompoundPropertyModel(req);
+	    CompoundPropertyModel<SearchRequest> searchReqModel = new CompoundPropertyModel<SearchRequest>(req);
 	    
-		TextField<String> cityTxt = new TextField<String>("city", searchReqModel.bind("city"));
-		TextField<String> areaTxt = new TextField<String>("area", searchReqModel.bind("area"));
-		TextField<Double> priceTxt = new TextField<Double>("price", searchReqModel.bind("price"));
-		TextField<Double> roomsFromTxt = new TextField<Double>("roomsFrom", searchReqModel.bind("roomsFrom"));
-		TextField<Double> roomsToTxt = new TextField<Double>("roomsTo", searchReqModel.bind("roomsTo"));
+	    Form<SearchRequest> searchForm = new Form<SearchRequest>("searchForm", searchReqModel);
+	    
+		TextField<String> cityTxt = new TextField<String>("city");
+		TextField<String> areaTxt = new TextField<String>("area");
+		TextField<Double> priceTxt = new TextField<Double>("price");
+		TextField<Double> roomsFromTxt = new TextField<Double>("roomsFrom");
+		TextField<Double> roomsToTxt = new TextField<Double>("roomsTo");
         
-		add(cityTxt);
-		add(areaTxt);
-		add(priceTxt);
-		add(roomsFromTxt);
-		add(roomsToTxt);
+		searchForm.add(cityTxt);
+		searchForm.add(areaTxt);
+		searchForm.add(priceTxt);
+		searchForm.add(roomsFromTxt);
+		searchForm.add(roomsToTxt);
+		searchForm.add(new AjaxButton("submitSearch") {
+
+			private static final long serialVersionUID = -8016115162670393962L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				System.out.println("Submitted values " + form.getModel().getObject());
+				
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+		});
+		
+		add(searchForm);
+		
+		
 		
 	}
 
--- ecom/src/main/java/com/ecom/web/search/SearchResultPage.java
+++ ecom/src/main/java/com/ecom/web/search/SearchResultPage.java
@@ -1,8 +1,14 @@
 package com.ecom.web.search;
 
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 
 public class SearchResultPage extends HomePage {
 
     private static final long serialVersionUID = -6983320790900379278L;
+    
+    public SearchResultPage(final PageParameters params) {
+
+    }
 
 }
