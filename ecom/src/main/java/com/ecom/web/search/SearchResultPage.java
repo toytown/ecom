5d83ccd8a271564d4d3ed553b1ac0b82d3f465ba
home <home@pt-laptop>
2011-10-11 06:26:15 +0200

    #added image handling

--- ecom/src/main/java/com/ecom/web/search/SearchResultPage.java
+++ ecom/src/main/java/com/ecom/web/search/SearchResultPage.java
@@ -1,5 +1,13 @@
 package com.ecom.web.search;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxButton;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
@@ -8,13 +16,19 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.image.NonCachingImage;
+import org.apache.wicket.markup.html.image.resource.RenderedDynamicImageResource;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.EmptyDataProvider;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.ecom.common.utils.ImageUtils;
 import com.ecom.domain.RealState;
 import com.ecom.web.components.pagination.ResultTablePagingNavigator;
 import com.ecom.web.data.RealStateDataProvider;
@@ -62,8 +76,15 @@ public class SearchResultPage extends HomePage {
             @Override
             protected void populateItem(Item<RealState> item) {
                 final RealState realState = (RealState) item.getModelObject();
-
-                item.add(new Label("title", realState.getTitle()));
+				Link detailImageLink = new Link("detailImageLink"){
+					@Override
+					public void onClick() {
+						//FIXME setResponsePage(new RealStateDetailView(advert));
+					}
+				};
+				detailImageLink.add(new Image("title_image", "D:/dev/gitRepository/ecom/ecom/src/main/webapp/images/p2.jpg"));
+				item.add(detailImageLink);
+				item.add(new Label("title", realState.getTitle()));
                 item.add(new Label("description", realState.getDescription()));
                 item.add(new Label("price", String.valueOf(realState.getCost())));
 
@@ -94,6 +115,14 @@ public class SearchResultPage extends HomePage {
                     protected void populateItem(Item<RealState> item) {
                         final RealState realState = (RealState) item.getModelObject();
 
+        				Link detailImageLink = new Link("detailImageLink"){
+        					@Override
+        					public void onClick() {
+        						//FIXME setResponsePage(new RealStateDetailView(advert));
+        					}
+        				};
+        				detailImageLink.add(getTitleImage());
+        				item.add(detailImageLink);                        
                         item.add(new Label("title", realState.getTitle()));
                         item.add(new Label("description", realState.getDescription()));
                         item.add(new Label("price", String.valueOf(realState.getCost())));
@@ -129,5 +158,46 @@ public class SearchResultPage extends HomePage {
         addOrReplace(searchForm);
 
     }
+    
+    
+	protected Image getTitleImage() {
+		return new NonCachingImage("title_image", new AbstractReadOnlyModel() {
+
+			@Override
+			public Object getObject() {
+				return new RenderedDynamicImageResource(50, 50) {
+
+					@Override
+					protected boolean render(Graphics2D g2) {
+
+						BufferedImage baseIcon = null;
+						BufferedImage baseIconOut = null;
+						try {
+							String imageFilePath = "D:/dev/gitRepository/ecom/ecom/src/main/webapp/images/p2.jpg";
+//							String imageFilePath = titleImageDir + File.separator + advert.getTitleImage();
+							File imageFile = new File(imageFilePath);
+							if (imageFile.canRead()) {
+								long startTime = System.currentTimeMillis();
+								baseIcon = ImageIO.read(imageFile);
+								long endTime = System.currentTimeMillis();
+								//logger.debug("Time taken to read image : " + (endTime - startTime) + " milliseconds.");
+								baseIconOut = ImageUtils.resize(baseIcon, 50, 50);
+								endTime = System.currentTimeMillis();
+								//logger.debug("Time taken to resize image : " + (endTime - startTime) + " milliseconds.");
+							} else {
+								throw new RuntimeException("No image file found at " + imageFilePath);
+							}
+
+						} catch (IOException e) {
+							throw new WicketRuntimeException(e);
+						}
+						g2.drawImage(baseIconOut, null, null);
+						return true;
+					}
+
+				};
+			}
+		});		
+	}    
 
 }
