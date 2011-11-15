86d847f0ade2f57d90c90ff7a3a0dfbbc8dcac56
home <home@192.168.1.33>
2011-11-15 17:29:07 +0100

    #changed the column layout

--- ecom/src/test/java/com/ecom/repository/RealStateRepositoryTest.java
+++ ecom/src/test/java/com/ecom/repository/RealStateRepositoryTest.java
@@ -49,13 +49,15 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 			String imageName = "test_image_1.jpg";
 
 			if (i % 2 == 0) {
-				appartment.setBalconyAvailable(false);
+				
 				appartment.setTotalRooms(2d);
 				appartment.setBedRooms(1);
 				appartment.setCost(600);
 				appartment.setSize(50.5d + 1);
+				appartment.setKitchenAvailable(true);
 				appartment.setProvisionFree(true);
-				appartment.setDescription("2 rooms appartment, schöne wohnlage");
+				appartment.setBalconyAvailable(false);
+				appartment.setDescription("2 rooms appartment, schöne wohnlage. Sehr gutes lage, ideal für couples.");
 				appartment.setStreet("Schlesierstr");
 				appartment.setHouseNo("4b");
 				appartment.setAreaCode("81667");
@@ -68,10 +70,11 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 				appartment.setTotalRooms(3d);
 				appartment.setBedRooms(2);
 				appartment.setCost(800);
-				appartment.setSize(67d + i * 1.1);
+				appartment.setSize(67d + i * 1.1);				
 				appartment.setKitchenAvailable(true);
 				appartment.setProvisionFree(true);
-				appartment.setDescription("3 zimmer appartment, EBK, familien freündlich");
+				appartment.setBalconyAvailable(true);
+				appartment.setTitle("3 zimmer appartment, EBK, familien freündlich. nahe von U -bhan. provision frei....");
 				appartment.setStreet("Schöhäuser allee");
 				appartment.setHouseNo("24 b");
 				appartment.setAreaCode("10337");
@@ -86,11 +89,10 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 				appartment.setSize(80 + i * 1.5);
 				appartment.setKitchenAvailable(true);
 				appartment.setBalconyAvailable(true);
-				appartment.setDescription("4 zimmer hell, ruhig, provisinfrei mit panoromablick");
-				appartment.setDescription("3 zimmer appartment, EBK, familien freündlich");
+				appartment.setTitle("4 zimmer hell, ruhig, provisinfrei mit panoromablick. Einbaukuche");
 				appartment.setStreet("Hauptstr");
 				appartment.setHouseNo("15");
-				appartment.setAreaCode("10337");
+				appartment.setAreaCode("10337");				
 				appartment.setCity("Frankfirt");
 				imageName = "test_image_" + i + ".jpg";
 
@@ -102,7 +104,7 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 				appartment.setSize(101 + i * 1.5);
 				appartment.setKitchenAvailable(true);
 				appartment.setBalconyAvailable(true);
-				appartment.setDescription("5 zimmer groß, ruhig, provisinfrei mit panoromablick");
+				appartment.setTitle("5 zimmer groß, ruhig, provisinfrei mit panoromablick. kaum zu vorstellbar.");
 				appartment.setStreet("Hamburgerstr");
 				appartment.setHouseNo("23 -37a");
 				appartment.setAreaCode("67789");
@@ -117,6 +119,7 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 				appartment.setSize(50.5d);
 				appartment.setKitchenAvailable(true);
 				appartment.setBalconyAvailable(true);
+				appartment.setTitle("5 zimmer groß, ruhig, provisinfrei mit panoromablick. kaum zu vorstellbar.");
 				appartment.setStreet("Neuhauser allee");
 				appartment.setHouseNo("22a");
 				appartment.setAreaCode("10117");
@@ -126,6 +129,9 @@ public class RealStateRepositoryTest extends AbstractIntegrationTest {
 
 			image = ImageIO.read(readImageFile("test-images/" + imageName));
 			appartment.setTitleImage(imageName);
+			
+			appartment.setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+			appartment.setAreaDescription("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
 			
 			appartment.setCategoryId(1);
 			appartment.setTypeId(1);
