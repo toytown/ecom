25e52ccf7ac1376f847df0f4d8e3f83fb119c7a1
home <home@192.168.1.33>
2012-02-24 21:21:39 +0100

    #added city in search criteria

--- ecom/src/main/java/com/ecom/service/impl/RealStateServiceImpl.java
+++ ecom/src/main/java/com/ecom/service/impl/RealStateServiceImpl.java
@@ -22,13 +22,17 @@ import com.mysema.query.types.Predicate;
 public class RealStateServiceImpl implements RealStateService<RealState> {
 
 	private static final long serialVersionUID = 7179912056888874311L;
-	
+
+	private static final double MIN_VAL = 0.0;
+
+	private static final double MAX_VAL = 9999999999.0;
+
 	@Autowired
 	private RealStateRepository realStateRepository;
 
 	@Autowired
 	private ImageService imageService;
-	
+
 	@Override
 	public Page<RealState> findBySearchRequest(SearchRequest req, PageRequest pageReq) {
 		return realStateRepository.findAll(buildPredicate(req), pageReq);
@@ -41,20 +45,26 @@ public class RealStateServiceImpl implements RealStateService<RealState> {
 		if (req == null) {
 			return builder.and(realStateQuery.id.eq(new ObjectId()));
 		}
-		
-        double areaFrom = req.getAreaFrom() != null ? req.getAreaFrom().doubleValue() : 0.0;
-        double areaTo = req.getAreaTo() != null ? req.getAreaTo().doubleValue() : 9999;   
-        builder.and(realStateQuery.size.between(areaFrom, areaTo));
-        
-		double roomsFrom = req.getRoomsFrom() != null ? req.getRoomsFrom().doubleValue() : 0.0;
-		double roomsTo = req.getRoomsTo() != null ? req.getRoomsTo().doubleValue() : 999;		
-		builder.and(realStateQuery.totalRooms.between(roomsFrom, roomsTo));
-		
-		
-		double priceFrom = req.getPriceFrom() != null ? req.getPriceFrom().doubleValue() : 0.0;
-        double priceTo = req.getPriceTo() != null ? req.getPriceTo().doubleValue() : 999999999;       
-        builder.and(realStateQuery.cost.between(priceFrom, priceTo));
 
+		if (StringUtils.isNotEmpty(req.getCity())) {
+			if (isZipCode(req.getCity())) {
+				builder.and(realStateQuery.areaCode.contains(req.getCity()));
+			} else {
+				builder.and(realStateQuery.city.containsIgnoreCase(req.getCity()));
+			}
+		}
+
+		double areaFrom = req.getAreaFrom() != null ? req.getAreaFrom().doubleValue() : MIN_VAL;
+		double areaTo = req.getAreaTo() != null ? req.getAreaTo().doubleValue() : MAX_VAL;
+		builder.and(realStateQuery.size.between(areaFrom, areaTo));
+
+		double roomsFrom = req.getRoomsFrom() != null ? req.getRoomsFrom().doubleValue() : MIN_VAL;
+		double roomsTo = req.getRoomsTo() != null ? req.getRoomsTo().doubleValue() : MAX_VAL;
+		builder.and(realStateQuery.totalRooms.between(roomsFrom, roomsTo));
+
+		double priceFrom = req.getPriceFrom() != null ? req.getPriceFrom().doubleValue() : MIN_VAL;
+		double priceTo = req.getPriceTo() != null ? req.getPriceTo().doubleValue() : MAX_VAL;
+		builder.and(realStateQuery.cost.between(priceFrom, priceTo));
 
 		if (req.isProvisionFree() != null && req.isProvisionFree().booleanValue() == true) {
 			builder.and(realStateQuery.provisionFree.eq(true));
@@ -63,29 +73,29 @@ public class RealStateServiceImpl implements RealStateService<RealState> {
 		if (req.isKitchenAvailable() != null && req.isKitchenAvailable().booleanValue() == true) {
 			builder.and(realStateQuery.kitchenAvailable.eq(true));
 		}
-		
+
 		if (req.isFurnished() != null && req.isFurnished() == true) {
 			builder.and(realStateQuery.furnished.eq(true));
 		}
-		
+
 		if (req.isBalconyAvailable() != null && req.isBalconyAvailable() == true) {
 			builder.and(realStateQuery.balconyAvailable.eq(true));
 		}
-		
+
 		if (req.isLiftAvailable() != null && req.isLiftAvailable() == true) {
 			builder.and(realStateQuery.liftAvailable.eq(true));
 		}
-		
+
 		if (req.isGardenAvailable() != null && req.isGardenAvailable().booleanValue()) {
 			builder.and(realStateQuery.garageAvailable.eq(true));
-		}		
+		}
 		return builder;
 	}
 
 	public Predicate buildPredicate(String userId, String filterVal) {
 		QRealState realStateQuery = new QRealState("realStateUser");
 		BooleanBuilder builder = new BooleanBuilder();
-		
+
 		Predicate condition = null;
 		if (!StringUtils.isEmpty(filterVal) && !StringUtils.isEmpty(userId)) {
 			builder.or(realStateQuery.city.contains(filterVal));
@@ -100,7 +110,9 @@ public class RealStateServiceImpl implements RealStateService<RealState> {
 		return condition;
 	}
 
-
+	public boolean isZipCode(String value) {
+		return StringUtils.isNotEmpty(value) && StringUtils.isNumeric(value);
+	}
 
 	@Override
 	public int count(SearchRequest req) {
@@ -109,7 +121,7 @@ public class RealStateServiceImpl implements RealStateService<RealState> {
 
 	@Override
 	public Page<RealState> findByUserSearchFilter(String userId, String filter, PageRequest pageReq) {
-		return realStateRepository.findAll(buildPredicate(userId, filter), pageReq);		
+		return realStateRepository.findAll(buildPredicate(userId, filter), pageReq);
 	}
 
 	@Override
@@ -121,19 +133,19 @@ public class RealStateServiceImpl implements RealStateService<RealState> {
 	public void deleteRealState(RealState realState) {
 		imageService.deleteAllImages(realState);
 		realStateRepository.delete(realState);
-		
+
 	}
 
 	@Override
 	public void activateRealState(RealState realState, Date activationDate) {
 		realState.activate(activationDate);
 		realStateRepository.save(realState);
-		
+
 	}
 
 	@Override
 	public void saveOrUpdate(RealState realState) {
 		realStateRepository.save(realState);
-		
+
 	}
 }
--- ecom/src/main/java/com/ecom/web/main/HomePage.java
+++ ecom/src/main/java/com/ecom/web/main/HomePage.java
@@ -9,8 +9,7 @@ import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.ecom.domain.OfferType;
@@ -21,18 +20,19 @@ import com.ecom.web.search.SearchResultPage;
 public class HomePage extends GenericTemplatePage {
 
 	private static final long serialVersionUID = 6571024929658974042L;
-    private static final List<OfferType> offerTypeList = Arrays.asList(OfferType.Rent, OfferType.Buy);
-    private static final List<RealStateType> realStateObjectList = Arrays.asList(RealStateType.Appartment, RealStateType.House, RealStateType.FurnishedAppartment, RealStateType.Land, RealStateType.Garage);
-    
+	private static final List<OfferType> offerTypeList = Arrays.asList(OfferType.Rent, OfferType.Buy);
+	private static final List<RealStateType> realStateObjectList = Arrays.asList(RealStateType.Appartment, RealStateType.House,
+			RealStateType.FurnishedAppartment, RealStateType.Land, RealStateType.Garage);
+
 	public HomePage() {
 		super();
 
 		SearchRequest req = new SearchRequest();
 		CompoundPropertyModel<SearchRequest> searchReqModel = new CompoundPropertyModel<SearchRequest>(req);
-		
+
 		final StatelessForm<SearchRequest> searchForm = new StatelessForm<SearchRequest>("searchForm", searchReqModel);
 		setStatelessHint(true);
-		
+
 		TextField<String> cityTxt = new TextField<String>("city");
 		TextField<String> areaTxt = new TextField<String>("areaFrom");
 		TextField<Double> priceTxt = new TextField<Double>("priceTo");
@@ -42,19 +42,19 @@ public class HomePage extends GenericTemplatePage {
 		searchForm.add(areaTxt);
 		searchForm.add(priceTxt);
 		searchForm.add(roomsFromTxt);
-		
-		//typeId - Rent or Buy
-        final IModel<OfferType> offerTypeModel = Model.of(req.getTypeId());
-        RadioChoice<OfferType> offerType = new RadioChoice<OfferType>("typeId", offerTypeModel, offerTypeList);
-        searchForm.add(offerType);
-        
-        //Realstate type - appartment, house, garage
-        final IModel<RealStateType> realStateTypeSel = Model.of(req.getRealStateType());
-       // EnumChoiceRenderer<RealStateType> realStateTypeEnum = new EnumChoiceRenderer<RealStateType>(this);
-        
-        DropDownChoice<RealStateType> realStateTypeDropdown = new DropDownChoice<RealStateType>("realStateType", realStateTypeSel, realStateObjectList);
-        searchForm.add(realStateTypeDropdown);
-        
+
+		// typeId - Rent or Buy
+		PropertyModel<OfferType> offer = new PropertyModel<OfferType>(req, "typeId");
+		RadioChoice<OfferType> offerType = new RadioChoice<OfferType>("typeId", offer, offerTypeList);
+		searchForm.add(offerType);
+
+		// Realstate type - appartment, house, garage
+		// EnumChoiceRenderer<RealStateType> realStateTypeEnum = new
+		// EnumChoiceRenderer<RealStateType>(this);
+
+		DropDownChoice<RealStateType> realStateTypeDropdown = new DropDownChoice<RealStateType>("realStateType", new PropertyModel<RealStateType>(req, "realStateType"), realStateObjectList);
+		searchForm.add(realStateTypeDropdown);
+
 		searchForm.add(new Button("submitSearch") {
 
 			private static final long serialVersionUID = -8016115162670393962L;
@@ -63,7 +63,6 @@ public class HomePage extends GenericTemplatePage {
 			public void onSubmit() {
 
 				SearchRequest req = (SearchRequest) searchForm.getDefaultModel().getObject();
-
 				PageParameters params = new PageParameters();
 				params.set("city", req.getCity());
 				params.set("areaFrom", req.getAreaFrom());
@@ -71,7 +70,7 @@ public class HomePage extends GenericTemplatePage {
 				params.set("roomsFrom", req.getRoomsFrom());
 				params.set("typeId", req.getTypeId().toString());
 				params.set("realStateType", req.getRealStateType().toString());
-				
+
 				setResponsePage(SearchResultPage.class, params);
 
 			}
