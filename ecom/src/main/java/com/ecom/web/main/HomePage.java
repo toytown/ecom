bd5c4771f1c0f2139cb07e4c8d1ee21122cfc212
home <home@pt-laptop>
2012-04-06 05:56:17 +0200

    - bug fixes

--- ecom/src/main/java/com/ecom/web/main/HomePage.java
+++ ecom/src/main/java/com/ecom/web/main/HomePage.java
@@ -12,12 +12,10 @@ import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import com.ecom.domain.OfferType;
 import com.ecom.domain.RealStateType;
 import com.ecom.domain.SearchRequest;
-import com.ecom.service.interfaces.GeoLocationService;
 import com.ecom.web.search.SearchResultPage;
 
 public class HomePage extends GenericTemplatePage {
@@ -27,9 +25,6 @@ public class HomePage extends GenericTemplatePage {
     private static final List<RealStateType> realStateObjectList = Arrays.asList(RealStateType.Appartment, RealStateType.House,
             RealStateType.FurnishedAppartment, RealStateType.Land, RealStateType.Garage);
 
-
-    @SpringBean
-    private GeoLocationService geoLocationService;
     
     public HomePage() {
         super();
@@ -42,36 +37,7 @@ public class HomePage extends GenericTemplatePage {
 
         IModel<String> cityModel = searchReqModel.bind("city");
         TextField<String> cityTxt = new TextField<String>("city",  cityModel);
-        
 
-        /*
-        TagItTextField<String> cityTxt = new TagItTextField<String>("city",  cityModel) {
-
-            @Override
-            public Iterable<String> getChoices(String input) {
-                List<String> emptyList = Collections.emptyList();
-                if (Strings.isEmpty(input)) {
-                    return emptyList;
-                }
-
-                Set<String> choices = new HashSet<String>(10);
-                Iterator<GeoLocation> iter = geoLocationService.findByZipOrCity(input).iterator();
-                while (iter.hasNext()) {
-                    GeoLocation geoLoc = iter.next();
-
-                    if (choices.size() == 10) {
-                        break;
-                    }
-
-                    choices.add(geoLoc.getCity());
-                }
-
-              
-                return choices;
-            }
-            
-        };
-        */
         TextField<String> areaTxt = new TextField<String>("areaFrom");
         TextField<Double> priceTxt = new TextField<Double>("priceTo");
         TextField<Double> roomsFromTxt = new TextField<Double>("roomsFrom");
@@ -107,8 +73,8 @@ public class HomePage extends GenericTemplatePage {
                 params.set("areaFrom", req.getAreaFrom());
                 params.set("priceTo", req.getPriceTo());
                 params.set("roomsFrom", req.getRoomsFrom());
-                params.set("typeId", req.getTypeId().toString());
-                params.set("realStateType", req.getRealStateType().toString());
+                params.set("typeId", OfferType.getId(req.getTypeId()));
+                params.set("realStateType", RealStateType.getId(req.getRealStateType()));
 
                 setResponsePage(SearchResultPage.class, params);
 
--- ecom/src/main/java/com/ecom/web/user/UserDashBoardPage.java
+++ ecom/src/main/java/com/ecom/web/user/UserDashBoardPage.java
@@ -2,15 +2,10 @@ package com.ecom.web.user;
 
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.image.ContextImage;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
-import com.ecom.domain.QUser;
 import com.ecom.domain.User;
-import com.ecom.repository.UserRepository;
+import com.ecom.web.login.LoginPage;
 import com.ecom.web.main.EcomSession;
 import com.ecom.web.main.GenericTemplatePage;
 import com.ecom.web.utils.SecurePage;
@@ -21,59 +16,19 @@ public class UserDashBoardPage extends GenericTemplatePage implements SecurePage
 
 	private static final long serialVersionUID = 8655494738660715236L;
 
-	@SpringBean
-	private UserRepository userRepository;
 
 	public UserDashBoardPage() {
 
 		EcomSession sess = (EcomSession) Session.get();
-		final String userName = sess.getUserName();
 
-		IModel<User> userModel = new LoadableDetachableModel<User>() {
+		if (!sess.isSignedIn()) {
+			setResponsePage(LoginPage.class);
+		}
+		
+		BookmarkablePageLink<User> editLink = new BookmarkablePageLink<User>("editLink", EntriesPage.class);
+		BookmarkablePageLink<User> accountLink = new BookmarkablePageLink<User>("accountLink", AccountPage.class);
+		BookmarkablePageLink<User> messagesLink = new BookmarkablePageLink<User>("messagesLink", MessageInboxPage.class);
 
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected User load() {
-				QUser user = new QUser("user");
-				return userRepository.findOne(user.userName.eq(userName));
-			}
-		};
-
-		Link<User> editLink = new Link<User>("editLink", userModel) {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onClick() {
-				PageParameters params = new PageParameters();
-				params.add("userId", this.getModel().getObject().getId().toString());
-				setResponsePage(EntriesPage.class);
-			}
-
-		};
-
-		Link<User> accountLink = new Link<User>("accountLink", userModel) {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onClick() {
-				setResponsePage(AccountPage.class);
-			}
-
-		};
-
-		Link<User> messagesLink = new Link<User>("messagesLink", userModel) {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onClick() {
-				setResponsePage(MessageInboxPage.class);
-			}
-
-		};
 
 		accountLink.add(new ContextImage("accountIcon", "images/usericons/user.png"));
 		editLink.add(new ContextImage("editIcon", "images/usericons/edit.png"));
